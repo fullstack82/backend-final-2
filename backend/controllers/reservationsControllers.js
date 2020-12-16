90a6e572843d0e6f955b77e10dbd0c9555582f72
fullstack82 <fullstack8210@gmail.com>
2020-12-16 15:57:26 +0100

    reservations

--- backend/controllers/reservationsControllers.js
+++ backend/controllers/reservationsControllers.js
@@ -1,1 +1,20 @@
+const { database } = require('../structure');
 
+
+
+async function getReservations(req, res) {
+    try {
+        const [reservations] = await database.pool.query('SELECT * FROM reservations');
+        res.send(experiences);
+    } catch (err) {
+        res.status(500);
+        res.send({ error: err.message });
+    }
+}
+
+
+
+
+module.exports = {
+    getReservations
+}
